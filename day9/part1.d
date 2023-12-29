6d3d4bfee14d89eee6ffb50e8f0f801908b1256e
Kaio Vieira <kaiomassevieira@gmail.com>
2023-12-29 16:19:29 -0300

    AoC day 9 (D) - cool :)

---
+++ day9/part1.d
@@ -0,0 +1,32 @@
+import std.stdio, std.array, std.algorithm, std.conv;
+
+auto get_next(long[] v) {
+	if (v.length == 1) return v[0];
+
+	bool allzero = true;
+	for (int i = 1; i < v.length; i++) {
+		allzero &= v[i] == 0;
+	}
+	if (allzero) return 0;
+
+	long[] next = new long[v.length - 1];
+	for (int i = 0; i + 1 < v.length; i++) {
+		next[i] = v[i + 1] - v[i];
+	}
+
+	return get_next(next) + v[v.length - 1];
+}
+
+void main() {
+	stdin
+		.byLineCopy
+		.array
+		.map!(a => a
+				.split
+				.map!(b => to!long(b))
+				.array
+				)
+		.map!(a => get_next(a))
+		.reduce!((a,b) => a + b)
+		.writeln;
+}
